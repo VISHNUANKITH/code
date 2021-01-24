8266d588a6174c660b2ec3cc6c6756343cd84376
VISHNUANKITH <77912615+VISHNUANKITH@users.noreply.github.com>
2021-01-24 12:03:35 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int pass;
+    printf(" 1 no to pass maths\n 2 no to pass maths\n 3 no to pass both\n");
+    scanf("%d",&pass);
+    if(pass==1)
+    printf("u got 50 chocalates");
+    else if(pass=2)
+    {
+    printf("u fot 10 chocalatwes");
+    }
+    else if(pass=3)
+    {
+    printf("u fot 310 chocalatwes");
+    } 
+}
